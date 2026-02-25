3f6056b8774dea73aae70703756f439f83cf088c
psw0962 <psw0962@naver.com>
2026-02-25 19:49:53 +0900

    chore: 프로젝트 셋팅

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Home from './Home';
+import Privacy from './Privacy';
+import Terms from './Terms';
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/privacy" element={<Privacy />} />
+      <Route path="/terms" element={<Terms />} />
+    </Routes>
+  );
+}
